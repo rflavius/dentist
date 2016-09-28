915351dc19c1c19c1aceac18de2364edece729d5
Flavius Rosu <rflavius@gmail.com>
2016-09-28 10:54:34 +0300

    allow access for the mandrill test file only based on dev IP

--- test_mandrill.php
+++ test_mandrill.php
@@ -3,6 +3,13 @@
  * here we will test the mandrill library
  */
 
+$allowedIPs = array('89.34.170.32');
+
+if(!in_array($_SERVER['REMOTE_ADDR'], $allowedIPs))
+{
+	die('NOT allowed !');
+}
+
 require_once 'library/mandrillEmails.php';
 
 $email = new mandrillEmails();
