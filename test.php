fcc771226879a10a647ec8a94b856decc163187c
Flavius Rosu <rflavius@gmail.com>
2016-10-03 10:36:50 +0300

    made some changes to start using mandrill app

--- test.php
+++ test.php
@@ -3,18 +3,14 @@
  * here we will test the mandrill library
  */
 
-require_once('vendor/autoload.php');
-
 $allowedIPs = array('89.34.170.32');
 if(!in_array($_SERVER['REMOTE_ADDR'], $allowedIPs))
 {
 	die('NOT allowed !');
 }
 
-$m = new Mandrill('JHxkBlCuxOCLY9Pm9npwag');
-print_r($m);exit;
-
+require_once('vendor/autoload.php');
 require_once 'library/mandrillEmails.php';
-$email = new mandrillEmails();
 
+$email = new mandrillEmails();
 print_r($email->getUserInfo());
